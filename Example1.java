ae593e76aebbfa1f196ea555ea5734b2721f796f
preethish-pk <97346956+preethish-pk@users.noreply.github.com>
2022-01-08 21:06:49 +0530

    Update Example1.java

--- Example1.java
+++ Example1.java
@@ -3,5 +3,30 @@ class Main
     public static void main(String args[])
     {
         System.out.println("Hi what's up");
+        int a=5;
+        a=5*4;
+        a=9;
+        System.out.println("The answer is ="+a);
+        if(a==9)
+        {
+            System.out.println("The ans is true");
+        }
+        else
+        System.out.println("Its false");
+        System.out.println("Repeat");
+        
+        int x,y;
+        x=5;
+        y=8;
+        if(x<y)
+        
+            System.out.println("Y is greater than x");
+        
+        if (x>y)
+        
+            System.out.println("X is greater that Y");
+        
+        if (x==y)
+        System.out.println("X and Y are equal");
     }
 }
